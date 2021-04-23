74bae9edc29750186f06177a3efad5a1d2f3a940
githcorrado <githcorrado@gmail.com>
2021-04-23 09:54:42 +0800

    completed all Goals in the book.

--- calculator.go
+++ calculator.go
@@ -1,13 +1,54 @@
 // Package calculator provides a library for simple calculations in Go.
 package calculator
 
+import (
+	"fmt"
+	"math"
+)
+
 // Add takes two numbers and returns the result of adding them together.
 func Add(a, b float64) float64 {
-return a + b
+	return a + b
+}
+
+func AddMany(inputs ...float64) float64 {
+	var acc float64 = 0
+	for _, input := range inputs {
+		acc = acc + input
+	}
+	return acc
 }
 
 // Subtract takes two numbers and returns the result of subtracting the second
 // from the first.
 func Subtract(a, b float64) float64 {
 	return b - a
+}
+
+func SubtractMany(inputs ...float64) float64 {
+	var acc float64
+	for _, input := range inputs {
+		acc = acc + input
+	}
+	return acc
+}
+
+func Multiply(a, b float64) float64 {
+	return a * b
+}
+
+func Divide(a, b float64) (float64, error) {
+	if b != 0 {
+		return a / b, nil
+	} else {
+		return 0, fmt.Errorf("bad input: %f, %f (division by zero is undefined)", a, b)
+	}
+}
+
+func Sqrt(a float64) (float64, error) {
+	if a >= 0 {
+		return math.Sqrt(a), nil
+	} else {
+		return 0, fmt.Errorf("bad input: %f (square root of a negative number is invalid", a)
+	}
 }
--- calculator_test.go
+++ calculator_test.go
@@ -2,23 +2,118 @@ package calculator_test
 
 import (
 	"calculator"
+	"math"
 	"testing"
 )
 
+type testCase struct {
+	a, b float64
+	want float64
+}
+
+type testCaseDivide struct {
+	a, b float64
+	want float64
+	errExcepted bool
+}
+
 func TestAdd(t *testing.T) {
 	t.Parallel()
-	var want float64 = 4
-	got := calculator.Add(2, 2)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+	testCases := []testCase{
+		{a: 2, b: 2, want: 4},
+		{a: 1, b: 5, want: 6},
+		{a: 2, b: -2, want: 0},
+	}
+	for _, tc := range testCases {
+		got := calculator.Add(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("want %f, got %f", tc.want, got)
+		}
 	}
 }
 
-// func TestSubtract(t *testing.T) {
-// 	t.Parallel()
-// 	var want float64 = 2
-// 	got := calculator.Subtract(4, 2)
-// 	if want != got {
-// 		t.Errorf("want %f, got %f", want, got)
-// 	}
-// }
+func TestSubtract(t *testing.T) {
+	t.Parallel()
+	testCases := []testCase{
+		{a: 2, b: 2, want: 0},
+		{a: 1, b: 5, want: 4},
+		{a: 2, b: -2, want: -4},
+	}
+	for _, tc := range testCases {
+		got := calculator.Subtract(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("want %f, got %f", tc.want, got)
+		}
+	}
+
+}
+
+func TestMultiply(t *testing.T) {
+	t.Parallel()
+	testCases := []testCase{
+		{a: 2, b: 2, want: 4},
+		{a: 1, b: 5, want: 5},
+		{a: 2, b: -2, want: -4},
+	}
+	for _, tc := range testCases {
+		got := calculator.Multiply(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("want %f, got %f", tc.want, got)
+		}
+	}
+}
+
+func TestDivide(t *testing.T) {
+	t.Parallel()
+	testCases := []testCaseDivide {
+		{a: 2, b: 2, want: 1, errExcepted: false},
+		{a: 1, b: 5, want: 0.2, errExcepted: false},
+		{a: 2, b: -2, want: -1, errExcepted: false},
+		{a: 2, b: 0, want: 0, errExcepted: true},
+		{a: 3, b: 0, want: 0, errExcepted: true},
+		{a: 4, b: 0, want: 0, errExcepted: true},
+	}
+
+	for _, tc := range testCases {
+		got, err := calculator.Divide(tc.a, tc.b)
+		errReceived := err != nil
+		if tc.errExcepted != errReceived {
+			t.Fatalf("Divide(%f, %f): unexpected error status: %v", tc.a, tc.b, errReceived)
+		}
+		if !tc.errExcepted && got != tc.want {
+			t.Errorf("want %f, got %f", tc.want, got)
+		}
+	}
+}
+
+func TestSqrt(t *testing.T) {
+	type testCase struct {
+		a float64
+		want float64
+		errExpected bool
+	}
+	t.Parallel()
+	testCases := []testCase {
+		{a: 4, want: 2, errExpected: false},
+		{a: 100, want: 10, errExpected: false},
+		{a: 0, want: 0, errExpected: false},
+		{a: -0, want: 0, errExpected: false},
+		{a: -4, want: 0, errExpected: true},
+		{a: 21, want: 4.582576, errExpected: false},
+	}
+	for _, tc := range testCases {
+		got, err := calculator.Sqrt(tc.a)
+		errReceived := err != nil
+		if tc.errExpected != errReceived {
+			t.Fatalf("Sqrt(%f): Unexpected error status: %v", tc.a, errReceived)
+		}
+		if !closeEnough(got, tc.want, 0.0001) {
+			t.Errorf("want: %f, got %f, precision 0.0001", tc.want, got)
+		}
+	}
+
+}
+
+func closeEnough(a, b, tolerance float64) bool {
+	return math.Abs(a - b) <= tolerance
+}
